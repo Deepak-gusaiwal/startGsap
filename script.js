f1da346adeaac253b9823dfe59e56724572041c7
Deepak <dgusaiwal537@gmail.com>
2023-07-10 23:45:18 +0530

    add scroll downl button

--- script.js
+++ script.js
@@ -30,3 +30,14 @@ tl.from(".heroBtnGroup span ",{
     delay:0.5,
     stagger:0.3,
 })
+
+tl.from(".ScrollDown",{
+    scale:0,
+    opacity:0,
+})
+tl.to(".ScrollDown",{
+    y:20,
+    repeat:-1,
+    yoyo:true,
+    scale:0.8
+})
